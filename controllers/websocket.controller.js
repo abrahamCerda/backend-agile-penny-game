66d4da45b993d58f30a375ac8e0c671d5df2f1cd
Abraham Cerda <abraham.cerda@usach.cl>
2020-08-28 09:27:36 -0400

    Update

--- controllers/websocket.controller.js
+++ controllers/websocket.controller.js
@@ -3,5 +3,8 @@ module.exports = function(httpServer) {
 
     io.on('connection', socket => {
         console.log('A new user connected');
+        socket.on('disconnect', () => {
+            console.log('User disconnected');
+        })
     });
 };
